5ed99836475f307db6dcc8a1502fb800f4344c3e
VillageLukaChen <villagelukaschen@gmail.com>
2016-03-20 09:53:23 +0000

    add navigation

---
+++ _javascript/directives/off-content-sidebar.js
@@ -0,0 +1,31 @@
+/*global angular*/
+angular.module('offContentSidebar',[])
+.directive('offContentSidebar',function($window) {
+    return {
+        restrict: 'A',
+        link: function(scope,element,attrs) {
+            var offsetTop = parseInt(attrs.offContentSidebar);
+            
+            var windowEl = angular.element($window);
+            
+            scope.isDisplay = 'none';
+            scope.displayed = 'isHidden';
+            
+            windowEl.on('scroll',function() {
+               if(windowEl.scrollTop() > offsetTop) {
+                   if(scope.displayed != 'isDisplayed') {
+                       scope.isDisplay = 'block';
+                       scope.displayed = 'isDisplayed';
+                       scope.$apply();
+                   }
+               } else {
+                   if(scope.displayed != 'isHidden') {
+                       scope.isDisplay = 'none';
+                       scope.displayed = 'isHidden';
+                       scope.$apply();
+                   }
+               }
+            });
+        }
+    }
+});
