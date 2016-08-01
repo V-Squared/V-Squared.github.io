5344a29cd112a36f49c6c4ecf8fee698babb83d5
lukaschen <villagelukaschen@gmail.com>
2016-08-01 23:59:15 +0800

    update

---
+++ _javascript/dependencies/switcher.js
@@ -0,0 +1,147 @@
+var switcher = {
+  bindings: {
+    "isOpen": "<?"
+  },
+  controller: swithercontroller,
+  transclude:true,
+  template: '<div ng-transclude></div>'
+};
+
+function swithercontroller ($scope,$element) {
+  var $ctrl = this;
+
+  $ctrl.texts = [];
+  $ctrl.items = [];
+  $ctrl.isOpen = $ctrl.isOpen || 0;
+
+  var previousIndex;
+
+  $ctrl.addText = addText;
+  $ctrl.addItem = addItem;
+
+  $ctrl.select = select;
+
+  function addText (text) {
+    console.log("ADD TEXT!!");
+    $ctrl.texts.push({
+      text: text,
+      index: text.index
+    });
+  }
+
+  function addItem (item,index) {
+    $ctrl.items.push({
+      item: item,
+      index: index
+    })
+  }
+
+  function select (index) {
+    if(previousIndex != undefined) {
+      console.log("previousIndex:",previousIndex);
+      var previousTextActive = $ctrl.texts[previousIndex],
+        previousItemActive = findItemIndex(previousIndex);
+
+      console.log("previousItemActive:",previousItemActive);
+
+      if(previousTextActive) {
+        previousTextActive.text.active = false;
+        console.log(previousTextActive);
+      }
+      if(previousItemActive) {
+        previousItemActive.item.active = false;
+      }
+    }
+
+    var selectedText = $ctrl.texts[index],
+      selectedItem = findItemIndex(index);
+
+    console.log($ctrl.items);
+
+    if (selectedText) {
+      console.log("This Index:",index);
+      selectedText.text.active = true;
+      selectedItem.item.active = true;
+      previousIndex = index;
+    }
+  }
+
+  function findItemIndex (index) {
+    for (var i = 0;i < $ctrl.items.length; i++) {
+      if($ctrl.items[i].index == index) {
+        return $ctrl.items[i];
+      }
+    }
+  }
+
+  $ctrl.$postLink = function () {
+    $element.addClass("switcher");
+    $scope.$watch('$ctrl.isOpen',select);
+  };
+}
+
+swithercontroller.$inject = ["$scope","$element"];
+
+function switchItem () {
+  var directive = {
+    restrict: "A",
+    require: "^v2Switcher",
+    scope:true,
+    link: switchItemLink
+  };
+  return directive;
+
+  function switchItemLink(scope,element,attrs,ctrl) {
+    var index = scope.$eval(attrs.v2SwitchItem);
+
+    ctrl.addItem(scope,index);
+
+    scope.active = false;
+
+    element.on("click",handleClick);
+
+    function handleClick () {
+      scope.$apply(function() {
+        ctrl.select(index);
+      });
+    }
+
+    scope.$watch('active',function(value) {
+      if(value) {
+        element.addClass('active')
+      } else {
+        element.removeClass("active");
+      }
+    });
+  }
+}
+
+var switchText = {
+  require: {
+    switcher: "^v2Switcher"
+  },
+  controller: switchTextController,
+  transclude:true,
+  template: "<md-card><md-card-content ng-transclude></md-card-content></md-card>"
+};
+
+function switchTextController ($scope,$element) {
+  this.$onInit = function onInit () {
+    this.switcher.addText(this);
+  };
+
+  this.$postLink = function () {
+    $scope.$watch("$ctrl.active",function(newVal) {
+      var display = !!newVal ? 'block' : 'none';
+      $element.css('display',display);
+    })
+  }
+}
+
+switchTextController.$inject = ["$scope","$element"];
+
+angular
+  .module("v2.switcher",[])
+  .component("v2Switcher",switcher)
+  .directive("v2SwitchItem",switchItem)
+  .component("v2SwitchText",switchText);
